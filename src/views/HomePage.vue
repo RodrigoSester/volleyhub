587847997427f4bfa8ba8f0436663057d0bb0296
Rodrigo Weber Sesterheim <rsesterheim@maisaedu.com.br>
2024-10-01 21:59:10 -0300

    feat: implement cards to show notifications, next matches and reminders

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,12 +1,91 @@
 <template>
-  <ion-page>
-    <ion-header>
-      <ion-toolbar>
-        <ion-title>Home page</ion-title>
-      </ion-toolbar>
-    </ion-header>
+  <ion-page style="margin: 16px 16px 0px 16px;">
     <ion-content>
-      <ion-button @click="openMenu">Open Menu</ion-button>
+      <ion-title style="margin-bottom: 24px">
+        Olá, {{ user.name }}!
+      </ion-title>
+      <ion-card>
+        <ion-card-header>
+          <ion-card-title>
+            Próximas partidas
+          </ion-card-title>
+          <ion-card-subtitle>
+            Confira as próximas partidas
+          </ion-card-subtitle>
+        </ion-card-header>
+        <ion-card-content>
+          <ion-list>
+            <ion-item>
+              <ion-label>
+                <h2>Partida 1</h2>
+                <p>Local: Quadra 1</p>
+                <p>Data: 01/01/2022</p>
+              </ion-label>
+            </ion-item>
+            <ion-item>
+              <ion-label>
+                <h2>Partida 2</h2>
+                <p>Local: Quadra 2</p>
+                <p>Data: 02/01/2022</p>
+              </ion-label>
+            </ion-item>
+          </ion-list>
+        </ion-card-content>
+      </ion-card>
+
+      <ion-card>
+        <ion-card-header>
+          <ion-card-title>
+            Notificação
+          </ion-card-title>
+          <ion-card-subtitle>
+            Confira as notificações
+          </ion-card-subtitle>
+        </ion-card-header>
+        <ion-card-content>
+          <ion-list>
+            <ion-item>
+              <ion-label>
+                <h2>Notificação 1</h2>
+                <p>Descrição da notificação 1</p>
+              </ion-label>
+            </ion-item>
+            <ion-item>
+              <ion-label>
+                <h2>Notificação 2</h2>
+                <p>Descrição da notificação 2</p>
+              </ion-label>
+            </ion-item>
+          </ion-list>
+        </ion-card-content>
+      </ion-card>
+
+      <ion-card>
+        <ion-card-header>
+          <ion-card-title>
+            Lembretes
+          </ion-card-title>
+          <ion-card-subtitle>
+            Confira os lembretes
+          </ion-card-subtitle>
+        </ion-card-header>
+        <ion-card-content>
+          <ion-list>
+            <ion-item>
+              <ion-label>
+                <h2>Lembrete 1</h2>
+                <p>Descrição do lembrete 1</p>
+              </ion-label>
+            </ion-item>
+            <ion-item>
+              <ion-label>
+                <h2>Lembrete 2</h2>
+                <p>Descrição do lembrete 2</p>
+              </ion-label>
+            </ion-item>
+          </ion-list>
+        </ion-card-content>
+      </ion-card>
     </ion-content>
   </ion-page>
 </template>
@@ -16,6 +95,13 @@ import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'HomePage',
+  data() {
+    return {
+      user: {
+        name: 'John Doe',
+      },
+    }
+  },
   methods: {
     openMenu() {
       console.log('Open menu');
