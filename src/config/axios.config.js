ce4830a72c4129414c32b89ad592ee106ce024e8
Rodrigo Weber Sesterheim <rsesterheim@maisaedu.com.br>
2024-10-04 22:07:03 -0300

    feat: implement handlers to interceptors

--- src/config/axios.config.js
+++ src/config/axios.config.js
@@ -1,17 +1,55 @@
 import axios from "axios";
+import storageConfig from "./storage.config";
 
-export const axiosInstance = axios.create({
-  baseURL: "https://volleyhub-api.onrender.com/api",
-  timeout: 30000,
-  headers: {
-    'Content-Type': 'application/json',
-  },
-});
+storageConfig.create();
 
-export const setToken = (token) => {
-  axiosInstance.defaults.headers.common.Authorization = `Bearer ${token}`;
+const _requestHandler = async request => {
+  request.requestStartTime = Date.now();
+  request.headers.Authorization = await storageConfig.get('authorization');
+  return request;
 };
 
-export const isAuthenticated = () => {
-  return !!axiosInstance.defaults.headers.common.Authorization;
+const _errorHandler = error => {
+  if (error?.response?.status === 404) {
+    return Promise.reject(error.response);
+  }
+
+  error.message = error?.response?.data?.error?.message;
+
+  return Promise.reject(error);
+};
+
+const _successHandler = response => {
+  const requestTime = Date.now() - response.config.requestStartTime;
+  console.log('requestTime', requestTime);
+  return response;
+};
+
+const customAxios = () => {
+  const instance = axios.create({
+    headers: {
+      Accept         : 'application/json',
+      'Content-Type' : 'application/json',
+    },
+    baseURL          : "http://localhost:3000/api",
+  });
+
+  instance.interceptors.request.use(request => _requestHandler(request));
+
+  instance.interceptors.response.use(
+    response => _successHandler(response),
+    error => _errorHandler(error)
+  );
+
+  return instance;
+};
+
+export const axiosInstance = customAxios();
+
+export const setToken = async (token) => {
+  await storageConfig.set('authorization', token);
+};
+
+export const isAuthenticated = async () => {
+  return await storageConfig.get('authorization');
 };
