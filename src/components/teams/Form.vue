5731329978ebacf648589983193e423141ae6e26
Rodrigo Weber Sesterheim <rsesterheim@maisaedu.com.br>
2024-10-05 16:53:57 -0300

    [WIP]style:

--- src/components/teams/Form.vue
+++ src/components/teams/Form.vue
@@ -6,26 +6,60 @@
           <ion-button @click="cancel()">Cancel</ion-button>
         </ion-buttons>
         <ion-title>Adicionar time</ion-title>
-        <ion-buttons slot="end">
-          <ion-button @click="cancel()">Criar</ion-button>
-        </ion-buttons>
       </ion-toolbar>
     </ion-header>
     <ion-content>
       <ion-card>
         <ion-card-content>
-          <ion-item>
-            <ion-label position="floating">Nome</ion-label>
-            <ion-input></ion-input>
-          </ion-item>
-          <ion-item>
-            <ion-label position="floating">Modalidade</ion-label>
-            <ion-input></ion-input>
-          </ion-item>
-          <ion-item>
-            <ion-label position="floating">Descrição</ion-label>
-            <ion-input></ion-input>
-          </ion-item>
+          <ion-grid>
+            <ion-row>
+              <ion-col size="12">
+                <ion-label position="floating">Título:</ion-label>
+                <ion-input
+                  style="margin-top: 4px"
+                  placeholder="Título do time"
+                  fill="outline"
+                  required
+                  @input="inputTeamValue($event.target.value, 'name')"
+                />
+              </ion-col>
+              <ion-col size="12">
+                <ion-label position="floating">Abreviação:</ion-label>
+                <ion-input
+                  style="margin-top: 4px"
+                  placeholder="Abreviação do time"
+                  fill="outline"
+                  required
+                  @input="inputTeamValue($event.target.value, 'abbreviation')"
+                />
+              </ion-col>
+              <ion-col size="12">
+                <!-- usar select -->
+                <ion-label position="floating">Modalidade:</ion-label>
+                <ion-input></ion-input>
+              </ion-col>
+              <ion-col size="12">
+                <ion-label>Link de imagem da sua bandeira:</ion-label>
+                <ion-input
+                  style="margin-top: 4px"
+                  placeholder="Link da imagem"
+                  fill="outline"
+                  required
+                  @input="inputTeamValue($event.target.value, 'flag_url')"
+                />
+              </ion-col>
+              <ion-col size="12">
+                <ion-label>Mensalidade:</ion-label>
+                <ion-input
+                  style="margin-top: 4px"
+                  placeholder="R$ 0,00"
+                  fill="outline"
+                  required
+                  @input="inputTeamValue($event.target.value, 'monthly_fee')"
+                />
+              </ion-col>
+            </ion-row>
+          </ion-grid>
           <ion-button expand="block">Adicionar</ion-button>
         </ion-card-content>
       </ion-card>
@@ -35,12 +69,21 @@
 
 <script>
 import { defineComponent } from 'vue';
+import { axiosInstance } from '../../config/axios.config';
+import { showToast } from '../../helper/toast.helper';
 
 export default defineComponent({
   name: 'ModalRegisterTeam',
   data() {
     return {
       isOpen: false,
+      team: {
+        name: '',
+        abbreviation: '',
+        modality: '',
+        flag_url: '',
+        monthly_fee: 0,
+      },
     };
   },
   methods: {
@@ -53,6 +96,26 @@ export default defineComponent({
     cancel() {
       this.close();
     },
+    inputTeamValue(value, key) {
+      this.team[key] = value;
+    },
+    async registerTeam() {
+      this.loading = true;
+
+      try {
+        const body = {
+          ...this.team,
+        };
+
+        const response = await axiosInstance.post('/teams', body);
+
+        console.log(response.data);
+        this.close();
+      } catch (error) {
+        console.error(error);
+        showToast('Erro ao adicionar time');
+      }
+    }
   },
 });
 </script>
