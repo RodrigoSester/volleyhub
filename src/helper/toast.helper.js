d0a5d8c876e41b534c974ccdda927d19616cc4b0
Rodrigo Weber Sesterheim <rsesterheim@maisaedu.com.br>
2025-03-10 23:21:11 -0300

    feat(toast): refactor toast helper to support headers and colors

--- src/helper/toast.helper.js
+++ src/helper/toast.helper.js
@@ -1,12 +1,27 @@
 
 import { toastController } from "@ionic/vue";
 
-export const showToast = async (message, position = 'bottom', duration = 2000) => {
+const _showToast = async ({ header = '', color = '', message, position = 'bottom', duration = 2000 }) => {
   const toast = await toastController.create({
+    header,
     message,
     duration,
     position,
+    color,
+    animated: true,
   });
  
   toast.present();
+};
+
+export const showToast = async (message, position = 'bottom', duration = 2000) => {
+  await _showToast({ message, position, duration });
+}
+
+export const showErrorToast = async (message) => {
+  await _showToast({ header: 'Error:', message, position: 'bottom', color: 'danger' });
+};
+
+export const showSuccessToast = async (message) => {
+  await _showToast({ message, position: 'bottom', color: 'success' });
 }
