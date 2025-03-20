40dc96b7564a60d13d0eaa340e061328df630998
Rodrigo Weber Sesterheim <rsesterheim@maisaedu.com.br>
2025-03-20 17:55:22 -0300

    feat: enhance footer style

--- src/views/IndexPage.vue
+++ src/views/IndexPage.vue
@@ -4,17 +4,18 @@
       <ion-router-outlet />
     </ion-content>
 
-    <ion-footer>
-      <ion-toolbar>
-        <ion-tab-bar>
+    <ion-footer class="footer">
+      <ion-toolbar class="footer__toolbar">
+        <ion-tab-bar class="footer__tab-bar">
           <ion-tab-button 
+            class="footer__tab-bar__button"
             v-for="page in appPages" 
             :key="page.id"
             :tab="page.title"
             :href="page.path"
             @click="goToPage(page.path)"
           >
-            <ion-icon :icon="page.icon" color="primary" />
+            <ion-icon :icon="page.icon" class="ion-no-margin footer__tab-bar__button__icon" />
             <ion-label>{{ page.title }}</ion-label>
           </ion-tab-button>
         </ion-tab-bar>
@@ -80,9 +81,32 @@ export default defineComponent({
       ]
     },
     goToPage(path) {
-      console.log("🚀 ~ goToPage ~ path:", path)
       this.$router.push(path);
     }
   }
 });
 </script>
+<style scoped lang="scss">
+.footer {
+  &__tab-bar {
+    border: 0 !important;
+    background: var(--ion-background-color) !important;
+
+    &__button {
+      border: 1px solid var(--ion-background-color-800) !important;
+
+      &:first-child {
+        border-top-left-radius: 8px;
+      }
+
+      &:last-child {
+        border-top-right-radius: 8px;
+      }
+
+      &__icon {
+        color: var(--ion-text-color);
+      }
+    }
+  }
+}
+</style>
